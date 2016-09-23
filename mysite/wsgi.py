a60b0b13f00fe0466a947233a93cfd686719347a
ravindranene <ravindranene@hotmail.com>
2016-09-23 20:05:13 +0530

    Updated as per details in the tutorial

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -6,11 +6,13 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
-
 import os
+import sys
+path = '/home/ravindranene/my-first-blog'  # use your own PythonAnywhere username here
 
+if path not in sys.path:
+    sys.path.append(path)
+os.environ['DJANGO_SETTINGS_MODULE'] = 'djangogirls.mysite.settings'
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
-
-application = get_wsgi_application()
+from django.contrib.staticfiles.handlers import StaticFilesHandler
+application = StaticFilesHandler(get_wsgi_application())
