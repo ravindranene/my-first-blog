b56da11432e7215d137f810dcb83ec3bbcb7b6f6
Ravindra Nene <Ravindra Nene>
2016-10-09 20:53:15 +0530

    update on 9th oct

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
+
+# https://www.pythonanywhere.com/forums/topic/2246/#id_post_34665
+
 # Create your views here.
+def post_list(request):
+    #posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html',{})
+
+
+
